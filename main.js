010f9fce5c5257415d38cce252408aff2f7ce5ce
Raphaël M-P <rmarquispelletier@gmail.com>
2017-06-28 13:17:07 -0400

    add base files

---
+++ main.js
@@ -0,0 +1,7 @@
+/* MAIN GAME JS */
+
+var canvas = document.getElementById("main");
+var bg = document.getElementById("bg");
+
+canvas.width  = window.innerWidth;
+canvas.height = window.innerHeight;

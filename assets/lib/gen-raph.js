55ac297ce11a971778111b082d475e4dc506bd5d
Raphaël M-P <rmarquispelletier@gmail.com>
2017-06-28 12:57:36 -0400

    added howler.js

---
+++ assets/lib/gen-raph.js
@@ -0,0 +1,15 @@
+function rdnm(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min +1)) + min;
+}
+
+// Converts from degrees to radians.
+Math.radians = function(degrees) {
+  return degrees * Math.PI / 180;
+};
+
+// Converts from radians to degrees.
+Math.degrees = function(radians) {
+  return radians * 180 / Math.PI;
+};

30afb067b189623f7a64c814e4271980cea07d42
Raphaël M-P <rmarquispelletier@gmail.com>
2017-06-28 17:38:45 -0400

    Added canvas and basic drawing

--- main.js
+++ main.js
@@ -3,5 +3,28 @@
 var canvas = document.getElementById("main");
 var bg = document.getElementById("bg");
 
-canvas.width  = window.innerWidth;
-canvas.height = window.innerHeight;
+width = 1000; // or window.innerWidth
+height = 750; //window.innerHeight
+
+canvas.width = width;
+canvas.height = height;
+bg.width  = width;
+bg.height = height;
+
+var ctx = canvas.getContext("2d");
+
+game = {
+  state: 'play',
+  scene: 'menu'
+}
+
+function draw(){
+  ctx.clearRect(0, 0, width, height);
+  ctx.fillStyle = "#b5e9ff";
+  ctx.fillRect(0, 0, width, height);
+  ctx.fillStyle = "red"
+  ctx.fillRect(100, 100, 100, 100)
+
+}
+
+setInterval(draw, 1000/30);

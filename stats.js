f82972e474eb883b959e463a59c349acdf3f35f5
Sendrix2000 <montcalmt@gmail.com>
2017-06-28 20:35:58 -0400

    added stats

---
+++ stats.js
@@ -0,0 +1,14 @@
+var gilles = {
+    bhp: 100,
+    bmp: 100,
+    str: 10,
+    const: 10,
+    will: 10,
+    int: 10,
+    hp: bhp*const/10,
+    mp: bmp*will/10,
+    atk: str,
+    def: str/2+const/2,
+    matk: int,
+    mdef: int/2+will/2,
+}
